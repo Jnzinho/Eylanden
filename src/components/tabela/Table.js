d696191385b1bb657007473a6df4b8abdffe9f7e
Jnzinho <joaovitorjn@unochapeco.edu.br>
2023-05-24 17:20:34 -0300

    Made a table component for characters

---
+++ src/components/tabela/Table.js
@@ -0,0 +1,35 @@
+import './table.css';
+
+function Table({ personagem }) {
+  return (
+    <div className="table-container">
+      <img
+        className="char-pic"
+        alt="kick"
+        src="https://cdn.donmai.us/original/e1/65/e165cc7311d8c63edc96ab17ccb8989a.jpg"
+      ></img>
+      <table>
+        <tbody>
+          <tr>
+            <th>Terra Natal:</th>
+            <td align="right">{personagem.terraNatal}</td>
+          </tr>
+          <tr>
+            <th>Raça:</th>
+            <td align="right">{personagem.raca}</td>
+          </tr>
+          <tr>
+            <th>Classe:</th>
+            <td align="right">{personagem.classe}</td>
+          </tr>
+          <tr>
+            <th>Antecedente:</th>
+            <td align="right">{personagem.antecedente}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
--- src/pages/Characters/Kick/KickPage.js
+++ src/pages/Characters/Kick/KickPage.js
@@ -1,5 +1,23 @@
+import Table from '../../../components/tabela/Table';
+
 function Kick() {
-  return <div>Kick!</div>;
+  const tableInfo = {
+    // <td>{personagem.terraNatal}</td>
+    //       <td>{personagem.raca}</td>
+    //       <td>{personagem.classe}</td>
+    //       <td>{personagem.antecedente}</td>
+    terraNatal: 'Feenin',
+    raca: 'Halfling',
+    classe: 'Gunslinger',
+    antecedente: 'Mecânico',
+  };
+
+  return (
+    <div>
+      <h1>Kick Lalafell</h1>
+      <Table personagem={tableInfo}></Table>
+    </div>
+  );
 }
 
 export default Kick;
